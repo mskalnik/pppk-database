38fcc3f67c34c98b6ac4cae83756969d183b89bd
Marino Skalnik <marino.skalnik@gmail.com>
2019-10-07 22:17:41 +0200

    Add formating

--- clean-database.sql
+++ clean-database.sql
@@ -22,17 +22,17 @@ GO
 
 CREATE TABLE CarBrand
 (
-	IDCarBrand	int IDENTITY,
-	[Name]		nvarchar(50),
+	IDCarBrand	            int IDENTITY,
+	[Name]		            nvarchar(50),
 	CONSTRAINT PK_IDCarBrand PRIMARY KEY (IDCarBrand)
 )
 GO
 
 CREATE TABLE CarType
 (
-	IDCarType	int IDENTITY,
-	[Name]		nvarchar(50),
-	CarBrandID	int,
+	IDCarType	            int IDENTITY,
+	[Name]		            nvarchar(50),
+	CarBrandID	            int,
 	CONSTRAINT PK_IDCarModel PRIMARY KEY (IDCarType),
 	CONSTRAINT FK_CarModel_CarBrandID FOREIGN KEY (CarBrandID) REFERENCES CarBrand(IDCarBrand)
 )
@@ -40,11 +40,11 @@ GO
 
 CREATE TABLE Car
 (
-	IDCar				int IDENTITY,
-	Registration		nvarchar(20),
-	InitialKm			int,
-	YearOfProduction	int,
-	CarTypeID			int,
+	IDCar				    int IDENTITY,
+	Registration		    nvarchar(20),
+	InitialKm			    int,
+	YearOfProduction	    int,
+	CarTypeID			    int,
 	CONSTRAINT PK_IDCar PRIMARY KEY (IDCar),
 	CONSTRAINT FK_Car_CarTypeID FOREIGN KEY (CarTypeID) REFERENCES CarType(IDCarType)
 )
@@ -52,39 +52,39 @@ GO
 
 CREATE TABLE TravelOrderType
 (
-	IDTravelOrderType	int IDENTITY,
-	[Type]				nvarchar(50),
+	IDTravelOrderType	    int IDENTITY,
+	[Type]				    nvarchar(50),
 	CONSTRAINT PK_IDTravelOrderType PRIMARY KEY (IDTravelOrderType)
 )
 GO
 
 CREATE TABLE City
 (
-	IDCity		int IDENTITY,
-	[Name]		nvarchar(50),
+	IDCity		            int IDENTITY,
+	[Name]		            nvarchar(50),
 	CONSTRAINT PK_IDCity PRIMARY KEY (IDCity)
 )
 GO
 
 CREATE TABLE Fuel
 (
-	IDFuel	int IDENTITY,
-	[Name]	nvarchar(50),
+	IDFuel	                int IDENTITY,
+	[Name]	                nvarchar(50),
 	CONSTRAINT PK_IDFuel PRIMARY KEY (IDFuel)
 )
 GO
 
 CREATE TABLE TravelOrder
 (
-	IDTravelOrder		int IDENTITY,
-	Distance			int,
-	StartDate			date,
-	EndDate				date,
-	TravelOrderTypeID	int,
-	DriverID			int,
-	StartCityID			int,
-	FinishCityID		int,
-	CarID				int,
+	IDTravelOrder		    int IDENTITY,
+	Distance			    int,
+	StartDate			    date,
+	EndDate				    date,
+	TravelOrderTypeID	    int,
+	DriverID			    int,
+	StartCityID			    int,
+	FinishCityID		    int,
+	CarID				    int,
 	CONSTRAINT PK_IDTravelOrder PRIMARY KEY (IDTravelOrder),	
 	CONSTRAINT FK_TravelOrder_TravelOrderTypeID FOREIGN KEY (TravelOrderTypeID) REFERENCES TravelOrderType(IDTravelOrderType),	
 	CONSTRAINT FK_TravelOrder_DriverID FOREIGN KEY (DriverID) REFERENCES Driver(IDDriver),
@@ -96,13 +96,13 @@ GO
 
 CREATE TABLE Cost
 (
-	IDCost			int IDENTITY,
-	[Date]			date,
-	Quantity		int,		
-	Price			money,	
-	FuelID			int,
-	CityID			int,
-	TravelOrderID	int,
+	IDCost			        int IDENTITY,
+	[Date]			        date,
+	Quantity		        int,
+	Price			        money,
+	FuelID			        int,
+	CityID			        int,
+	TravelOrderID	        int,
 	CONSTRAINT PK_IDCost PRIMARY KEY (IDCost),
 	CONSTRAINT FK_Cost_FuelID FOREIGN KEY (FuelID) REFERENCES Fuel(IDFuel),	
 	CONSTRAINT FK_Cost_CityID FOREIGN KEY (CityID) REFERENCES City(IDCity),
@@ -112,14 +112,14 @@ GO
 
 CREATE TABLE [Route]
 (
-	IDRoute			int IDENTITY,
-    StartCoordinate decimal,
-    EndCoordinate	decimal,
-    StartDate		date,
-    EndDate			date,
-    Distance		decimal,
-    FuelUsed		decimal,
-    TravelOrderID	int,
+	IDRoute			        int IDENTITY,
+    StartCoordinate         decimal,
+    EndCoordinate	        decimal,
+    StartDate		        date,
+    EndDate			        date,
+    Distance		        decimal,
+    FuelUsed		        decimal,
+    TravelOrderID	        int,
 	CONSTRAINT PK_IDRoute PRIMARY KEY (IDRoute),
 	CONSTRAINT FK_Route_TravelOrderID FOREIGN KEY (TravelOrderID) REFERENCES TravelOrder(IDTravelOrder)
 )
--- create-database.sql
+++ create-database.sql
@@ -17,17 +17,17 @@ GO
 
 CREATE TABLE CarBrand
 (
-	IDCarBrand	int IDENTITY,
-	[Name]		nvarchar(50),
+	IDCarBrand	            int IDENTITY,
+	[Name]		            nvarchar(50),
 	CONSTRAINT PK_IDCarBrand PRIMARY KEY (IDCarBrand)
 )
 GO
 
 CREATE TABLE CarType
 (
-	IDCarType	int IDENTITY,
-	[Name]		nvarchar(50),
-	CarBrandID	int,
+	IDCarType	            int IDENTITY,
+	[Name]		            nvarchar(50),
+	CarBrandID	            int,
 	CONSTRAINT PK_IDCarModel PRIMARY KEY (IDCarType),
 	CONSTRAINT FK_CarModel_CarBrandID FOREIGN KEY (CarBrandID) REFERENCES CarBrand(IDCarBrand)
 )
@@ -35,11 +35,11 @@ GO
 
 CREATE TABLE Car
 (
-	IDCar				int IDENTITY,
-	Registration		nvarchar(20),
-	InitialKm			int,
-	YearOfProduction	int,
-	CarTypeID			int,
+	IDCar				    int IDENTITY,
+	Registration		    nvarchar(20),
+	InitialKm			    int,
+	YearOfProduction	    int,
+	CarTypeID			    int,
 	CONSTRAINT PK_IDCar PRIMARY KEY (IDCar),
 	CONSTRAINT FK_Car_CarTypeID FOREIGN KEY (CarTypeID) REFERENCES CarType(IDCarType)
 )
@@ -47,39 +47,39 @@ GO
 
 CREATE TABLE TravelOrderType
 (
-	IDTravelOrderType	int IDENTITY,
-	[Type]				nvarchar(50),
+	IDTravelOrderType	    int IDENTITY,
+	[Type]				    nvarchar(50),
 	CONSTRAINT PK_IDTravelOrderType PRIMARY KEY (IDTravelOrderType)
 )
 GO
 
 CREATE TABLE City
 (
-	IDCity		int IDENTITY,
-	[Name]		nvarchar(50),
+	IDCity		            int IDENTITY,
+	[Name]		            nvarchar(50),
 	CONSTRAINT PK_IDCity PRIMARY KEY (IDCity)
 )
 GO
 
 CREATE TABLE Fuel
 (
-	IDFuel	int IDENTITY,
-	[Name]	nvarchar(50),
+	IDFuel	                int IDENTITY,
+	[Name]	                nvarchar(50),
 	CONSTRAINT PK_IDFuel PRIMARY KEY (IDFuel)
 )
 GO
 
 CREATE TABLE TravelOrder
 (
-	IDTravelOrder		int IDENTITY,
-	Distance			int,
-	StartDate			date,
-	EndDate				date,
-	TravelOrderTypeID	int,
-	DriverID			int,
-	StartCityID			int,
-	FinishCityID		int,
-	CarID				int,
+	IDTravelOrder           int IDENTITY,
+	Distance		        int,
+	StartDate		        date,
+	EndDate			        date,
+	TravelOrderTypeID       int,
+	DriverID		        int,
+	StartCityID		        int,
+	FinishCityID	        int,
+	CarID			        int,
 	CONSTRAINT PK_IDTravelOrder PRIMARY KEY (IDTravelOrder),	
 	CONSTRAINT FK_TravelOrder_TravelOrderTypeID FOREIGN KEY (TravelOrderTypeID) REFERENCES TravelOrderType(IDTravelOrderType),	
 	CONSTRAINT FK_TravelOrder_DriverID FOREIGN KEY (DriverID) REFERENCES Driver(IDDriver),
@@ -91,13 +91,13 @@ GO
 
 CREATE TABLE Cost
 (
-	IDCost			int IDENTITY,
-	[Date]			date,
-	Quantity		int,		
-	Price			money,	
-	FuelID			int,
-	CityID			int,
-	TravelOrderID	int,
+	IDCost			        int IDENTITY,
+	[Date]			        date,
+	Quantity		        int,
+	Price			        money,
+	FuelID			        int,
+	CityID			        int,
+	TravelOrderID	        int,
 	CONSTRAINT PK_IDCost PRIMARY KEY (IDCost),
 	CONSTRAINT FK_Cost_FuelID FOREIGN KEY (FuelID) REFERENCES Fuel(IDFuel),	
 	CONSTRAINT FK_Cost_CityID FOREIGN KEY (CityID) REFERENCES City(IDCity),
@@ -107,14 +107,14 @@ GO
 
 CREATE TABLE [Route]
 (
-	IDRoute			int IDENTITY,
-    StartCoordinate float,
-    EndCoordinate	float,
-    StartDate		date,
-    EndDate			date,
-    Distance		float,
-    FuelUsed		float,
-    TravelOrderID	int,
+	IDRoute			    int IDENTITY,
+    StartCoordinate     float,
+    EndCoordinate	    float,
+    StartDate		    date,
+    EndDate			    date,
+    Distance		    float,
+    FuelUsed		    float,
+    TravelOrderID	    int,
 	CONSTRAINT PK_IDRoute PRIMARY KEY (IDRoute),
 	CONSTRAINT FK_Route_TravelOrderID FOREIGN KEY (TravelOrderID) REFERENCES TravelOrder(IDTravelOrder)
 )
